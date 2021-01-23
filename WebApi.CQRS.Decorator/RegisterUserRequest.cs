42c2ca4d19a0f270d05d10d9724783b4e9615e26
itaslure <lukasz.reszke@simployer.com>
2021-01-23 14:09:19 +0100

    Renamed CreateUserRequest to RegisterUserRequest

---
+++ WebApi.CQRS.Decorator/RegisterUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.CQRS.Decorator
+{
+    public class RegisterUserRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+    }
+}
